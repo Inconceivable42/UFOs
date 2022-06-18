d0550ff61c34a1cdf1fa20ca36309fe507af5856
Inconceivable42 <pdxmolly42@gmail.com>
2022-06-17 22:58:37 -0700

    forgot if updated

--- static/js/app.js
+++ static/js/app.js
@@ -27,3 +27,29 @@ data.forEach((dataRow) => {
   });
 }
 
+function handleClick() {
+
+  //Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+
+  let filteredData = tableData;
+
+  //Check to see if a date wa entered and filter the data using that date
+  //if statement for looking for data with given date
+  if (date) {
+    //Apply'filter' to the table data to only keep the rows
+    //where the 'datetime' value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  
+
+  //Rebuild the table using the filtered data
+
+  //@Note: If no date was entered, then filteredData will just be original tableData
+  buildTable(filteredData);
+}
+
+//Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
